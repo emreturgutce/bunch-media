e9b49419095c8d881877311ef9bf681a86b4d146
Emre Turgut <emrebursalee@gmail.com>
2021-02-08 18:37:39 +0300

    json parser, morgan, and disabling x-powered-by and etag

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
+import { router } from './routes';
 
 const app = express();
+
+app.disable('x-powered-by');
+app.disable('etag');
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use(router);
 
 export { app };
