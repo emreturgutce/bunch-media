33e108725b8d2a4884c498afb5ea8259e3e5f423
Emre Turgut <emrebursalee@gmail.com>
2021-02-09 13:15:39 +0300

    custom typings for session and cookie options

--- server/src/typings/custom.d.ts
+++ server/src/typings/custom.d.ts
@@ -1,10 +1,30 @@
+declare module 'express-session' {
+	interface Session {
+		context: {
+			id: string;
+		};
+	}
+}
+
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			NODE_ENV: string;
 			PORT: string;
 			MONGODB_URI: string;
+			REDIS_HOST: string;
+			REDIS_PASSWORD: string;
+			REDIS_PORT: string;
+			SESSION_SECRET: string;
+			JWT_SECRET: string;
 		}
+	}
+
+	interface CookieOptions {
+		sameSite: boolean | 'none' | 'strict' | 'lax' | undefined;
+		secure: boolean;
+		httpOnly: boolean;
+		signed: boolean;
 	}
 }
 
