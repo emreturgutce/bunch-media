f74b01f39186d64b3b6fe4ea94a7c05a8e163be7
Emre Turgut <emrebursalee@gmail.com>
2021-02-09 13:16:52 +0300

    exporting redis and session env vars in config/index

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -3,6 +3,16 @@ import Ajv from 'ajv';
 
 dotenv.config();
 
-export const ajv = new Ajv()
-
-export const { NODE_ENV, MONGODB_URI, PORT } = process.env;
+export const ajv = new Ajv();
+export * from './redis';
+export * from './session';
+export const {
+	NODE_ENV,
+	MONGODB_URI,
+	PORT,
+	REDIS_HOST,
+	REDIS_PASSWORD,
+	REDIS_PORT,
+	SESSION_SECRET,
+	JWT_SECRET,
+} = process.env;
