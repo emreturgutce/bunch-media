b1bf225da61d8247e8fccdbbd733a87b2b54cdde
Emre Turgut <emrebursalee@gmail.com>
2021-02-09 15:27:55 +0300

    sending confirmation mail after creating user

--- server/src/controllers/create-user.ts
+++ server/src/controllers/create-user.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
 import { MongoError } from 'mongodb';
 import { User } from '../models/user';
+import { createConfirmationUrl, sendEmail } from '../utils';
 
 export async function createUserController(
 	req: Request,
@@ -16,9 +17,16 @@ export async function createUserController(
 			message: 'User created',
 			data: { user },
 		});
+
+		await sendEmail(
+			req.body.email as string,
+			await createConfirmationUrl(user.id),
+		);
 	} catch (error) {
 		if (error instanceof MongoError) {
-			return next(new createHttpError.BadRequest(error.errmsg ?? 'Bad Request'));
+			return next(
+				new createHttpError.BadRequest(error.errmsg ?? 'Bad Request'),
+			);
 		}
 
 		next(new createHttpError.InternalServerError('Something went wrong'));
