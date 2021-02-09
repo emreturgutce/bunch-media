2fdb5affbb9d2d6283b3f6eead417b316a32f876
Emre Turgut <emrebursalee@gmail.com>
2021-02-09 16:45:53 +0300

    forgot password and change forgotten password controllers added

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import { changeForgottenPasswordController } from '../controllers/change-forgotten-password';
 import { confirmUserController } from '../controllers/confirm-user';
 import { createUserController } from '../controllers/create-user';
+import { forgotPasswordController } from '../controllers/forgot-password';
 import { getUsersController } from '../controllers/get-users';
 import { loginUserController } from '../controllers/login-user';
 import { logoutUserController } from '../controllers/logout-user';
 import { auth } from '../middlewares/auth';
 import { errorHandler } from '../middlewares/error-handler';
 import { notFound } from '../middlewares/not-found';
+import { validateChangeForgottenPassword } from '../middlewares/validate-change-forgotten-password';
 import { validateConfirmEmail } from '../middlewares/validate-confirm';
 import { validateLogin } from '../middlewares/validate-login';
+import { validateForgotPassword } from '../middlewares/validate-forgot-password';
 import { validateUser } from '../middlewares/validate-user';
 
 const router = Router();
@@ -18,6 +22,8 @@ router.post('/api/signup', validateUser, createUserController);
 router.post('/api/login', validateLogin, loginUserController);
 router.get('/api/logout', auth, logoutUserController);
 router.get('/api/confirm/:token', validateConfirmEmail, confirmUserController);
+router.post('/api/forgot-password', validateForgotPassword, forgotPasswordController);
+router.post('/api/change-password/:token', validateChangeForgottenPassword , changeForgottenPasswordController);
 
 router.use(notFound);
 router.use(errorHandler);
