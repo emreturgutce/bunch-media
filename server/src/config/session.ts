1a10ca6b0b107c8798ede73227c980bafaaa2449
Emre Turgut <emrebursalee@gmail.com>
2021-02-09 13:16:19 +0300

    session config

---
+++ server/src/config/session.ts
@@ -0,0 +1,33 @@
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { NODE_ENV, RedisClient, SESSION_SECRET } from '.';
+import { COOKIE_EXPIRATION, COOKIE_NAME } from '../constants';
+
+const RedisStore = connectRedis(session);
+
+const isProd = NODE_ENV === 'production';
+
+export const cookieOptions: CookieOptions = {
+	sameSite: isProd ? 'none' : 'strict',
+	secure: isProd,
+	httpOnly: true,
+	signed: true,
+};
+
+function sessionConfig() {
+	return session({
+		store: new RedisStore({
+			client: RedisClient.getInstance(),
+		}),
+		name: COOKIE_NAME,
+		secret: SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			...cookieOptions,
+			maxAge: COOKIE_EXPIRATION,
+		},
+	});
+}
+
+export { sessionConfig as session };
