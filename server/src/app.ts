d9c328fe4c2ea6b6f78a0f7f8c87d97b551b5784
Emre Turgut <emrebursalee@gmail.com>
2021-02-09 13:17:28 +0300

    using session

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import { session } from './config';
 import { router } from './routes';
 
 const app = express();
@@ -8,6 +9,7 @@ app.disable('x-powered-by');
 app.disable('etag');
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(session());
 
 app.use(router);
 
