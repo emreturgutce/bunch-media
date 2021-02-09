f422f0117a686abeaab6fe9f2e018ac572a0a7fb
Emre Turgut <emrebursalee@gmail.com>
2021-02-09 12:13:38 +0300

    login route added

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import { createUserController } from '../controllers/create-user';
 import { getUsersController } from '../controllers/get-users';
+import { loginUser } from '../controllers/login-user';
 import { errorHandler } from '../middlewares/error-handler';
 import { notFound } from '../middlewares/not-found';
+import { validateLogin } from '../middlewares/validate-login';
 import { validateUser } from '../middlewares/validate-user';
 
 const router = Router();
 
 router.get('/', getUsersController);
 router.post('/', validateUser, createUserController);
+router.post('/login', validateLogin, loginUser);
 
 router.use(notFound);
 router.use(errorHandler);
